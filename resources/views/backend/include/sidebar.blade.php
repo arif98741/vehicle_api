5cdc62876178a7b60f6e455e90802f844173253c
Ariful <arif98741@gmail.com>
2021-12-08 02:07:55 +0600

    change

--- resources/views/backend/include/sidebar.blade.php
+++ resources/views/backend/include/sidebar.blade.php
@@ -4,34 +4,203 @@
         <!-- Sidebar navigation-->
         <nav class="sidebar-nav">
             <ul id="sidebarnav" class="pt-4">
-
-
                 <li class="sidebar-item">
-                    <a
-                        class="sidebar-link has-arrow waves-effect waves-dark"
-                        href="javascript:void(0)"
-                        aria-expanded="false"
-                    ><i class="mdi mdi-glass-flute"></i
-                        ><span class="hide-menu">Labs </span></a
+                    <a class="sidebar-link has-arrow "
+                       href="javascript:void(0)"
+                       aria-expanded="false"
+                    ><i class="mdi mdi-view-sequential"></i
+                        ><span class="hide-menu">Service Order </span></a
                     >
                     <ul aria-expanded="false" class="collapse first-level">
                         <li class="sidebar-item">
                             <a href="#" class="sidebar-link"
                             ><i class="mdi mdi-plus"></i
-                                ><span class="hide-menu">Add Lab </span></a
+                                ><span class="hide-menu">Add Custom Order </span></a
                             >
                         </li>
                         <li class="sidebar-item">
                             <a href="#" class="sidebar-link"
                             ><i class="mdi mdi-format-list-bulleted"></i
-                                ><span class="hide-menu">Lab List </span></a
+                                ><span class="hide-menu">Order LIst </span></a
+                            >
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-format-list-bulleted"></i
+                                ><span class="hide-menu">Pending Order </span></a
+                            >
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-briefcase-check"></i
+                                ><span class="hide-menu">Completed Order </span></a
+                            >
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link has-arrow "
+                       href="javascript:void(0)"
+                       aria-expanded="false"
+                    ><i class="mdi mdi-cash"></i
+                        ><span class="hide-menu">Earning </span></a
+                    >
+                    <ul aria-expanded="false" class="collapse first-level">
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-format-list-numbers"></i
+                                ><span class="hide-menu">Earning List </span></a
+                            >
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-format-list-bulleted"></i
+                                ><span class="hide-menu">Pending Earning </span></a
+                            >
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-format-list-bulleted"></i
+                                ><span class="hide-menu">Processed Earning </span></a
+                            >
+                        </li>
+
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link has-arrow "
+                       href="javascript:void(0)"
+                       aria-expanded="false"
+                    ><i class="mdi mdi-briefcase"></i
+                        ><span class="hide-menu">Service list </span></a
+                    >
+                    <ul aria-expanded="false" class="collapse first-level">
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-briefcase-download"></i
+                                ><span class="hide-menu">Add Service </span></a
+                            >
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-briefcase-upload"></i
+                                ><span class="hide-menu">Service LIst </span></a
+                            >
+                        </li>
+                        <li class="sidebar-item dropdown-menu">
+                            <ul>
+                                <li class="sidebar-item">Hello</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link has-arrow "
+                       href="javascript:void(0)"
+                       aria-expanded="false"
+                    ><i class="mdi mdi-chart-histogram"></i
+                        ><span class="hide-menu">Report </span></a
+                    >
+                    <ul aria-expanded="false" class="collapse first-level">
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-checkbox-marked"></i
+                                ><span class="hide-menu">Service Report </span></a
+                            >
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-checkbox-marked"></i
+                                ><span class="hide-menu">Order Report </span></a
+                            >
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-checkbox-marked"></i
+                                ><span class="hide-menu">Earning Report </span></a
+                            >
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-checkbox-marked"></i
+                                ><span class="hide-menu">User Report </span></a
+                            >
+                        </li>
+
+                    </ul>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link has-arrow "
+                       href="javascript:void(0)"
+                       aria-expanded="false"
+                    ><i class="mdi mdi-account-multiple-outline"></i
+                        ><span class="hide-menu">User list </span></a
+                    >
+                    <ul aria-expanded="false" class="collapse first-level">
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-plus"></i
+                                ><span class="hide-menu">Add User </span></a
+                            >
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-format-list-bulleted"></i
+                                ><span class="hide-menu">User LIst </span></a
+                            >
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link has-arrow "
+                       href="javascript:void(0)"
+                       aria-expanded="false"
+                    ><i class="mdi mdi-account-settings-variant"></i
+                        ><span class="hide-menu">Role list </span></a
+                    >
+                    <ul aria-expanded="false" class="collapse first-level">
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-plus"></i
+                                ><span class="hide-menu">Add User Role </span></a
+                            >
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-format-list-bulleted"></i
+                                ><span class="hide-menu">Role LIst </span></a
+                            >
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link has-arrow waves-effect waves-dark"
+                       href="javascript:void(0)"
+                       aria-expanded="false"
+                    ><i class="mdi mdi-view-list"></i
+                        ><span class="hide-menu">Pages </span></a
+                    >
+                    <ul aria-expanded="false" class="collapse first-level">
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-plus"></i
+                                ><span class="hide-menu">Add Page </span></a
+                            >
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="#" class="sidebar-link"
+                            ><i class="mdi mdi-format-list-bulleted"></i
+                                ><span class="hide-menu">Page List </span></a
                             >
                         </li>
                     </ul>
                 </li>
                 <li class="sidebar-item">
                     <a
-                        class="sidebar-link waves-effect waves-dark" href="{{ url('logout') }}"><i class="mdi mdi-logout"></i
+                        class="sidebar-link waves-effect waves-dark" href="{{ url('logout') }}"><i
+                            class="mdi mdi-logout"></i
                         >Logout </a
                     >
                 </li>
