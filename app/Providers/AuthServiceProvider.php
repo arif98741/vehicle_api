e613e66d3474e57f988ca12a3976260362da0348
Ariful <arif98741@gmail.com>
2021-12-27 02:42:36 +0600

    token expiration time added

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Passport\Passport;
@@ -26,6 +27,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
+        Passport::personalAccessTokensExpireIn(Carbon::now()->addDay(15)); //this will be reduced in future
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
+
         Gate::before(function ($user, $ability) {
             return $user->hasRole('super admin') ? true : null;
         });
