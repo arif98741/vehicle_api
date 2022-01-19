2590b530a2479a4135973f76bce3143e2f1daceb
Ariful <arif98741@gmail.com>
2022-01-20 00:43:22 +0600

    frontend initiated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('guest');
     }
 
     /**
@@ -33,6 +33,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+
         return view('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return redirect('login');
-});
+Route::get('/','HomeController@index');
 
 Auth::routes();
 
